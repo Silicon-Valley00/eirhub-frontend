c43e60c8c853d6e18cc4700c2e6e0ca7318192d7
JhohannesK <Jdomeh77@gmail.com>
2022-06-29 02:09:38 +0000

    navbar responsivness for mobile and tablet screens

--- client/src/Pages/LandingPage/components/Navbar.js
+++ client/src/Pages/LandingPage/components/Navbar.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../logo.svg';
 import './navbar.css';
+import close from '../../../images/close.svg';
+import menu from '../../../images/menu.svg';
 
 const Navbar = () => {
+   const [sidebar, setSidebar] = useState(false);
+
+   const showSidebar = () => setSidebar(!sidebar);
+
    return (
       <nav id="nav">
+         {/* Logo */}
          <div id="img">
             <img src={logo} alt="" height={50} width={200} />
          </div>
-         <div>
-            <ul id="nav-links">
-               <li>Home</li>
-               <li>Our Services</li>
-               <li>Find a doctor</li>
-               <li>How it Works</li>
-            </ul>
+
+         <div className={sidebar ? 'link-container active' : 'link-container'}>
+            {/* Close icon */}
+            <div>
+               <img
+                  src={close}
+                  alt=""
+                  id="close"
+                  height={30}
+                  width={30}
+                  onClick={showSidebar}
+               />
+            </div>
+            <div className="left-div">
+               <ul id="nav-links">
+                  <li>Home</li>
+                  <li>Our Services</li>
+                  <li>Find a doctor</li>
+                  <li>How it Works</li>
+               </ul>
+               <div id="signup">
+                  <p>Sign Up | Login</p>
+               </div>
+            </div>
          </div>
-         <div>
-            <p>Sign Up | Login</p>
-         </div>
+
+         {/* menu icon */}
+         <img src={menu} alt="" height={30} width={30} onClick={showSidebar} />
       </nav>
    );
 };
