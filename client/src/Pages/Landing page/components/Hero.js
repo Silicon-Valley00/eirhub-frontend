348194af6bb478b1459bd91704accd09a94d9dfa
JhohannesK <Jdomeh77@gmail.com>
2022-07-01 02:38:48 +0000

    destop view hero section with css

--- client/src/Pages/Landing page/components/Hero.js
+++ client/src/Pages/Landing page/components/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => {
                </div>
             </div>
             <div className="hero-image">
-               <img src={landingpageImage} alt={'Hero-image'} />
+               <img src={landingpageImage} alt="Hero" />
             </div>
          </div>
       </section>
