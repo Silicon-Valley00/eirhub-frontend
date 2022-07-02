f6a41fe884e094145c356057e466b9ad6376d034
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-07-01 18:20:30 -0700

    Connect home route with remote database

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,53 @@
-from curses import echo
 import os
-from flask import Flask
-from sqlalchemy import create_engine
+from flask import Flask,jsonify
+from sqlalchemy import create_engine,Table,MetaData,Column,Integer,String
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+
 app = Flask(__name__)
 load_dotenv()
-engine = create_engine(os.getenv('dbconnectionstring'),echo=True)
+engine = create_engine( os.getenv('dbconnectionstring'))#establish a connection with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+meta = MetaData()
+Base = declarative_base()
+
+#Create a student class that translates into the student table
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer,primary_key = True)
+    name = Column('name',String(20))
+    course = Column('course',String(50))
+
+    def __init__ (self,name,course):
+        self.name = name
+        self.course = course
+
+
 try:
     engine.connect()
+    Base.metadata.create_all(engine)
+    new = Student("Albus severus Potter","Potions") #
+    new2 = Student('Leta Lestrange','DADA')
+    session.add(new)
+    session.add(new2)
+    session.commit()
+    session.close()
     print("Done")
 except Exception as e:
     print(e)
 # Echo for debugging for the moment
-@app.route("/home")
+
+@app.route("/home",methods = ['GET'])
 def home():
-    print(engine.table_names) #The engine connection. Testing per route
-    return ('Home: DB Connection Successful')
-    # Will abstract into new file.
+    session = Session()
+    print('Home: DB Connection Successful')
+    students = session.query(Student).all() 
+    for student in students:
+     return  student.name
+   
 
 
 
