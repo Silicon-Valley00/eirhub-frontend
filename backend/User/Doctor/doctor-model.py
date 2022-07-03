f2fded88bf6ad33e7271c55ae2e78b0fabf59602
Rexford Machu <rexfordmachu@gmail.com>
2022-07-03 17:21:11 +0000

    Add Person Database Model
    Update Database connection with Person Model

---
+++ backend/User/Doctor/doctor-model.py
@@ -0,0 +1,17 @@
+# from sqlalchemy import Column,Integer,String,Float,DateTime
+# from sqlalchemy.ext.declarative import declarative_base
+# Base = declarative_base()
+# class Doctor(Base):
+#     __tablename__ = "doctora"
+#     idDoctor = Column(Integer,primary_key = True)
+#     idHospital = Column(0,Integer)
+#     idPerson = Column(1,Integer)
+#     doctorRatings = Column(3.5,Float)
+#     doctorSpecialty = Column('Paediatrician',String(100))
+#     # createdAt = Column(get,DateTime)
+#     # updatedAt = Column('updated_at',DateTime)
+
+
+#     def __init__ (self,name,course):
+#         self.name = name
+#         self.course = course
