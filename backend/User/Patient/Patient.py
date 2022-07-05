b57eee9ceeb77f6f473ed27a27decb1a4b090466
kwabenadarkwa <darkwak@live.com>
2022-07-05 13:38:37 +0000

    edits Patient model

--- backend/User/Patient/Patient.py
+++ backend/User/Patient/Patient.py
@@ -1,3 +1,4 @@
+from enum import auto
 from sqlalchemy import Column,Integer,String,Date,ForeignKey,Float
 from sqlalchemy.orm import declarative_base,relationship
 
@@ -5,7 +6,7 @@ Base = declarative_base()
 
 class Patient(Base):
     __tablename__ = 'Patient'
-    idPatient = Column(Integer, primary_key=True, unique = True,nullable = False)
+    idPatient = Column(Integer, primary_key=True, unique = True,nullable = False, autoincrement = True)
     #these are Foreign keys to the primary keys of the health details and guardian tables:
     #documentation :  https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html
     health_details_id = Column(Integer, ForeignKey('Health_Details.health_details_id'),unique=True,nullable=True)
