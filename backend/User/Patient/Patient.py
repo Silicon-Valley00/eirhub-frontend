0b24521577649437fd5cdaebceb001ea7c17a4f7
kwabenadarkwa <darkwak@live.com>
2022-07-05 13:25:09 +0000

    edits Patient model

--- backend/User/Patient/Patient.py
+++ backend/User/Patient/Patient.py
@@ -5,11 +5,11 @@ Base = declarative_base()
 
 class Patient(Base):
     __tablename__ = 'Patient'
-    idPatient = Column(Integer, primary_key=True, unique = True)
+    idPatient = Column(Integer, primary_key=True, unique = True,nullable = False)
     #these are Foreign keys to the primary keys of the health details and guardian tables:
     #documentation :  https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html
-    health_details_id = Column(Integer, ForeignKey('Health_Details.health_details_id'),unique=True)
-    guardian_id = Column(Integer, ForeignKey('Guardian_Details.guardian_id'),unique=True)
+    health_details_id = Column(Integer, ForeignKey('Health_Details.health_details_id'),unique=True,nullable=True)
+    guardian_id = Column(Integer, ForeignKey('Guardian_Details.guardian_id'),unique=True,nullable=True)
     weight = Column(Float, nullable = True)
     height = Column(Float, nullable = True)
     location = Column(String(100),nullable = True)
