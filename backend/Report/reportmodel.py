a30f60212115a43ebb06c4d10af02d151277c152
Rexford Machu <rexfordmachu@gmail.com>
2022-07-09 11:24:38 +0000

    Update Report Model
    Complete Report Model based on current DB design.

--- backend/Report/reportmodel.py
+++ backend/Report/reportmodel.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column,Integer,String,ForeignKey
 from sqlalchemy.orm import relationship
-# Not complete. Defining Foreign Key relationship for Patient.
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 class Report(Base):
     __tablename__ = "Report"
     report_id = Column(Integer, primary_key=True, unique = True,nullable = False, autoincrement = True)
-    idPatient = Column(Integer,ForeignKey('Patient.idPatient', ondelete='CASCADE'))
-    idPatient = relationship('idPatient', backref='Patient')
+    idPatient = Column(Integer,ForeignKey('Patient.idPatient', nullable=False, ondelete='CASCADE'))
     report_type = Column('report_type',String(50))
     description = Column('description',String(200))
     medicine_and_dosage = Column('medicine_and_dosage',String(200))
