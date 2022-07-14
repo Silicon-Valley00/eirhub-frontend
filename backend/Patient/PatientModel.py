cd75b52d1ce2f69b6f173784d8e01f8d7e853c9c
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-07-14 13:32:53 -0700

    Update columns in patient model

--- backend/Patient/PatientModel.py
+++ backend/Patient/PatientModel.py
@@ -1,28 +1,30 @@
+from enum import unique
 from tkinter import CASCADE
 from sqlalchemy import Column,Integer,String,Date,ForeignKey,Float
 from sqlalchemy.orm import declarative_base,relationship
 
 
-
-
 Base = declarative_base()
+
+# class_associations = Table('patients_doctors',Base.metadata,Column("patient_id",ForeignKey = ))
 
 class Patient(Base):
     __tablename__ = 'Patient'
     idPatient = Column(Integer,primary_key= True,unique = True,autoincrement=True,nullable = False)
     idGuardianPerson = Column(Integer,ForeignKey("GuardianPerson.idGuardian"))
+    idDoctor = Column(Integer,)
     first_name = Column("first_name",String(60))
     middle_name = Column("middle_name",String(60))
     last_name = Column("last_name",String(60))
     person_image = Column("person_image",String(2083))
-    user_email = Column("user_email",String(50))
+    user_email = Column("user_email",String(50),unique = True)
     user_password = Column("user_password",String(200))
     date_of_birth = Column("date_of_birth",Date)
     house_address = Column("house_address",String(45))
     phone_number = Column("phone_number",String(45))
-    id_number = Column("id_number",String(45))
+    id_number = Column("id_number",String(45),unique = True)
     nationality = Column("nationality",String(50))
-    
+    gender = Column("gender",String(45))
 
     
 
