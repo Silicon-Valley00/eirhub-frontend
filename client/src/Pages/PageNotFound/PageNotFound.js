5784b30c7da7e3c7d123e574e2decad1c42ab23b
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2022-07-16 22:29:45 +0000

    Addin more styles and transistions to 404 page

--- client/src/Pages/PageNotFound/PageNotFound.js
+++ client/src/Pages/PageNotFound/PageNotFound.js
@@ -5,15 +5,13 @@ function PageNotFound() {
    return (
       <div className={PageNotFoundStyles.PNFcontainer}>
          <h1 className={PageNotFoundStyles.header1}>Eirhub</h1>
-         <div className={PageNotFoundStyles.PNFcontainer1}>
-            <div className={PageNotFoundStyles.Para}>
-               <h1>OOOPS</h1>
-               <p>We can't seem to find the page you are looking for.</p>
-               <button>Back to Home</button>
-            </div>
-            <div className={PageNotFoundStyles.PNFimage}>
-               <img src={NotFound}></img>
-            </div>
+         <div className={PageNotFoundStyles.Para}>
+            <h1>Oooops!!</h1>
+            <p>We can't seem to find the page you are looking for.</p>
+            <button>Back to Home</button>
+         </div>
+         <div className={PageNotFoundStyles.PNFimage}>
+            <img src={NotFound}></img>
          </div>
       </div>
    );
