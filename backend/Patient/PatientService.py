f97b0e660383af873f193bdddcb89b000544b719
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-07-16 11:43:26 -0700

    Add getPatientById to Patient API

--- backend/Patient/PatientService.py
+++ backend/Patient/PatientService.py
@@ -8,17 +8,29 @@ patients_route = Blueprint("patients_route",__name__)
 def getPatients():
     from app import session
     #trial object to test database entries
-    # newPatient = Patient("Jerry","Leo","Jackman","https:/myImage.com","Jackman@gmail.com","madeit","2022-7-15"
+    # newPatient = Patient("Terry","Meo","Lackman","https:/myImage.com","Lackman@gmail.com","madeit","2022-7-15"
     # ,"12 molly street","+2332467897","57849003","male","Ghanaian")
     # session.add(newPatient)
     # session.commit()
     # session.close()
     patients = session.query(Patient).all()
-    print(type(patients))
     for patient in patients:
-         return jsonify({
+         return ({
+            "id": patient.idPatient,
             "First name": patient.first_name,
             "Last_name": patient.last_name,
             "email": patient.user_email,
             "Nationality": patient.nationality
         })
+
+@patients_route.route("/patient/<id>",methods = ['GET'])
+def getPatientById(id):
+    from app import session
+    patient = session.query(Patient).get(id)
+    return ({
+            "First name": patient.first_name,
+            "Last_name": patient.last_name,
+            "email": patient.user_email,
+            "Nationality": patient.nationality
+        })
+
