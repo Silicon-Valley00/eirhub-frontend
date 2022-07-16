4342adc2f7f8c57061b91f5bd9dd435d88a9034f
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-07-17 00:11:10 +0100

    Add a doctor profile look up feature in the Find a doctor page for Users' dashboard

--- client/src/Pages/User Dashboard/components/FindDoctorProfile.js
+++ client/src/Pages/User Dashboard/components/FindDoctorProfile.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './finddoctorprofile.module.css';
+import doctorProfileOne from '../../../assets/docProfileImage3.svg';
 
 function FindingDoctor(props) {
    const [profile, setProfile] = useState('default');
@@ -8,7 +9,34 @@ function FindingDoctor(props) {
 
    return (
       <>
-         <div className={styles.profileBody}>{profile}</div>
+         <div className={styles.profileBody}>
+            <div className={styles.profileImage}>
+               <img src={doctorProfileOne} alt="doctor-profile" />
+            </div>
+            <div className={styles.profileDetails}>
+               <h3>Gucci Delix, Pharm D</h3>
+               <p>Clinical Pharmacist</p>
+            </div>
+            <div className={styles.profileInfo}>
+               <div className={styles.profileRatings}>
+                  <h3>Ratings</h3>
+                  <p>3 out of 5</p>
+               </div>
+               <div className={styles.profileSpecialities}>
+                  <h3>Specialities</h3>
+                  <p>Clinical Pharmacy</p>
+                  <p>Hermatology</p>
+                  <p>Oncology</p>
+               </div>
+               <div className={styles.profileHospital}>
+                  <h3>Hospital</h3>
+                  <p>Ridge Hospital</p>
+               </div>
+            </div>
+            <div className={styles.profileButton}>
+               <button>Request an Appointment</button>
+            </div>
+         </div>
       </>
    );
 }
