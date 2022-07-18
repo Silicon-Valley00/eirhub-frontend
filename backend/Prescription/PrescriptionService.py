31edc4b3c985ebff4704bcdb833556fbb3af8b51
kwabenadarkwa <darkwak@live.com>
2022-07-18 09:18:02 +0000

    finish prescription service

--- backend/Prescription/PrescriptionService.py
+++ backend/Prescription/PrescriptionService.py
@@ -1,20 +1,33 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,Blueprint,request
 from PrescriptionModel import Prescription
 from Patient.PatientModel import Patient 
 from flask_sqlalchemy import SQLAlchemy
+from app import session
+import json
 
-app = Flask(__name__)
+prescription_service_route = Blueprint("prescription_service_route",__name__)
 
-#function to get prescription of a patient 
-@app.route("/prescription/<int:idPatient>",methods = ["GET"])
+#get prescription of a patient with a partiular id
+@prescription_service_route.route("/prescription/<int:idPatient>",methods = ["GET"])
 def get_prescription(idPatient):
     prescription =  Prescription.query.get(idPatient)
     if prescription is None:
         return {'drug_name':None,'dosage':None,'time_of_administration':None,'start_date':None,'end_date':None,'last_taken_date':None}
     return jsonify(prescription)
 
-#post prescription of a patient
-@app.route("/prescription/<int:idPatient>",methods = ["POST"])
+
+#post prescription of a patient with particular id
+@prescription_service_route.route("/prescription/<int:idPatient>",methods = ["POST"])
 def post_prescription(idPatient):
-    pass
+    req = request.json
+    drug_name = req['drug_name']
+    dosage = req['dosage']
+    time_of_administration  = req['time_of_administration']
+    start_date  = req['start_date']
+    end_date = req['end_date']
+    last_taken_date  = req['last_taken_date']
+    
+    new_presciption = Prescription(drug_name,dosage,time_of_administration,start_date,end_date,last_taken_date)
+    session.add(new_presciption)
+    session.commit()
 #to be completed
