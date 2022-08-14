b3298b15333a3cb128c96c9b6515e71a0502eae4
JhohannesK <Jdomeh77@gmail.com>
2022-08-14 22:59:44 +0000

    increase height and width of dropdown

--- client/src/Pages/Landing page/components/LoginDropdown.js
+++ client/src/Pages/Landing page/components/LoginDropdown.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import styles from './dropdown.module.css';
 
-const LoginDropdown = ({ clicked,handleModalLogin,handleModalLoginDoctor }) => {
+const LoginDropdown = ({
+   clicked,
+   handleModalLogin,
+   handleModalLoginDoctor,
+}) => {
    return (
       <>
-      {/* FIXME: Increase the size of the dropdown*/}
          <div className={styles.main}>
             <div className={styles.sub_main}>
                <ul className={styles.list}>
                   <li className={styles.list_first}>Login as: </li>
-                  <div className={styles.list_last}                         onClick={() => handleModalLogin()}
->
+                  <div
+                     className={styles.list_last}
+                     onClick={() => handleModalLogin()}
+                  >
                      Patient
                   </div>
-                  <div className={styles.list_last}                         onClick={() => handleModalLoginDoctor()}
->
+                  <div
+                     className={styles.list_last}
+                     onClick={() => handleModalLoginDoctor()}
+                  >
                      Doctor
                   </div>
                </ul>
