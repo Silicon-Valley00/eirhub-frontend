1be4aa39f4b64461630aad28068471c4a68fcd33
Nii <niiodartey10@gmail.com>
2022-08-26 12:09:49 +0100

    implement file upload

--- client/src/Pages/DoctorDashboard/Records/DoctorRecords.js
+++ client/src/Pages/DoctorDashboard/Records/DoctorRecords.js
@@ -3,9 +3,25 @@ import Navigation from '../components/Navigation';
 import { Link } from 'react-router-dom';
 import { FaFileUpload, FaTrash, FaPencilAlt } from 'react-icons/fa';
 import Patients from '../DoctorPatients/DoctorPatients'
+import { useState, useRef } from 'react';
 
 
 function DoctorRecords() {
+   const [selectedFile, setSelectedFile] = useState()
+   const [isSelected, setIsSelected] = useState(false)
+   const docRecordsUploadRef = useRef()
+
+   const uploadFile = ()=> {
+      docRecordsUploadRef.current.click()
+   }
+   const changeHandler = (e)=> {
+      setSelectedFile(e.target.files[0])
+      setIsSelected(true)
+   }
+
+   const handleSubmission = ()=> {
+
+   }
    
 
    return (
@@ -15,12 +31,24 @@ function DoctorRecords() {
             <div className={styles.docRecordsContainer1}>
             <h1>Upload Files</h1>
             <div className={styles.docRecordsUpload}>
-               <FaFileUpload className={styles.docRecordsUploadimg} />
+               <input ref={docRecordsUploadRef} style={{display:'none'}} type="file" accept='.doc,.docx,.pdf,.txt' name='file' onChange={changeHandler}/>
+               {isSelected ? (
+                  <div>
+                     <p>Filename: {selectedFile.name}</p>
+                     <p>Filetype: {selectedFile.type}</p>
+                     <p>Size: {selectedFile.size/1024/1024}MB</p>
+                        <p>lastModifiedDate:{' '}
+                           {selectedFile.lastModifiedDate.toLocaleDateString()}</p>
+                     <button onClick= {handleSubmission}>Submit</button>
+                  </div>
+               ):
+               <FaFileUpload className={styles.docRecordsUploadimg} onClick={uploadFile} />}
                <h2 className={styles.docRecordsSheader}>
-                  Drag and drop file or{' '}
+                     Drag and drop file or <span className={styles.docRecordsButtonLink} onClick={uploadFile}>browse</span>
+                  {/* Drag and drop file or{' '}
                   <Link to="/" className={styles.docRecordsButtonLink}>
                      browse
-                  </Link>
+                  </Link> */}
                </h2>
             </div>
             <div className={styles.docRecordsFiles}>
