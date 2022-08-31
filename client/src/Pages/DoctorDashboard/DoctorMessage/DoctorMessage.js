b8af548475c17f2cca76e04be550cfa96b9f5030
Nii <niiodartey10@gmail.com>
2022-08-31 12:54:16 +0100

    fix input text color

--- client/src/Pages/DoctorDashboard/DoctorMessage/DoctorMessage.js
+++ client/src/Pages/DoctorDashboard/DoctorMessage/DoctorMessage.js
@@ -1,26 +1,26 @@
-// import React, { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import styles from './message.module.css';
-// import { CometChatMessages } from '../../../Chat UI Kit/cometchat-pro-react-ui-kit/CometChatWorkspace/src';
-// import MessagePatients from '../components/MessagePatients';
-// import Navigation from '../components/Navigation';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import styles from './message.module.css';
+import { CometChatMessages } from '../../../Chat UI Kit/cometchat-pro-react-ui-kit/CometChatWorkspace/src';
+import MessagePatients from '../components/MessagePatients';
+import Navigation from '../components/Navigation';
 
-// function Message() {
-//    const patientUID = useSelector((state) => state.patientToChatWith);
-//    return (
-//       <>
-//          <Navigation nav={10} />
-//          <main>
-//             <div className={styles.middle_section}>
-//                <div id={styles.messageBody}>
-//                   <CometChatMessages chatWithUser={patientUID} />
-//                </div>
-//             </div>
-//             <div className={styles.right_pane}>
-//                <MessagePatients />
-//             </div>
-//          </main>
-//       </>
-//    );
-// }
-// export default Message;
+function Message() {
+   const patientUID = useSelector((state) => state.patientToChatWith);
+   return (
+      <>
+         <Navigation nav={10} />
+         <main>
+            <div className={styles.middle_section}>
+               <div id={styles.messageBody}>
+                  <CometChatMessages chatWithUser={patientUID} />
+               </div>
+            </div>
+            <div className={styles.right_pane}>
+               <MessagePatients />
+            </div>
+         </main>
+      </>
+   );
+}
+export default Message;
