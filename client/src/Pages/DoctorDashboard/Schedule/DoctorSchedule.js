ebf79ccd3bc4839453212160917bfe4d858f0b9b
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2022-09-01 18:28:19 +0000

    add font size to input and padding to table

--- client/src/Pages/DoctorDashboard/Schedule/DoctorSchedule.js
+++ client/src/Pages/DoctorDashboard/Schedule/DoctorSchedule.js
@@ -89,27 +89,26 @@ const DoctorSchedule = (props) => {
    //    if (allAppointments.length !== 0) {
    //       allAppointments.map((data, index) => {
    //          return (
-   //             <tr key={index}>
-   //                <td className={DSstyles.imgSection}>
-   //                   <img src={data?.patient_info.person_image} alt={'img'} />
-   //                </td>
-   //                <td>
-   //                   {data?.patient_info.first_name}{' '}
-   //                   {data?.patient_info.last_name}{' '}
-   //                </td>
-   //                <td className={DSstyles.tdCondition}>
-   //                   Swollen tonsils with severe pains in throat and chest
-   //                </td>
-   //                <td
-   //                   style={{
-   //                      color: '#EC6464',
-   //                      cursor: 'pointer',
-   //                   }}
-   //                   onClick=""
-   //                >
-   //                   Cancel
-   //                </td>
-   //             </tr>
+   // <tr key={index}>
+   //    <td className={DSstyles.imgSection}>
+   //       <img src={data?.patient_info.person_image} alt={'img'} />
+   //    </td>
+   //    <td>
+   //       {data?.patient_info.first_name} {data?.patient_info.last_name}{' '}
+   //    </td>
+   //    <td className={DSstyles.tdCondition}>
+   //       Swollen tonsils with severe pains in throat and chest
+   //    </td>
+   //    <td
+   //       style={{
+   //          color: '#EC6464',
+   //          cursor: 'pointer',
+   //       }}
+   //       onClick=""
+   //    >
+   //       Cancel
+   //    </td>
+   // </tr>;
    //          );
    //       });
    //    } else if (allAppointments.length === 0) {
@@ -201,7 +200,7 @@ const DoctorSchedule = (props) => {
                            <th className={DSstyles.tAction}>Action</th>
                         </thead>
                         <tbody>
-                           {allAppointments.map((data, index) => {
+                           {/* {allAppointments.map((data, index) => {
                               return (
                                  <tr key={index}>
                                     <td
@@ -219,6 +218,7 @@ const DoctorSchedule = (props) => {
                                        onClick={() =>
                                           displaySelectedPatientDetails(index)
                                        }
+                                       className={DSstyles.tdName}
                                     >
                                        {data?.patient_info.first_name}{' '}
                                        {data?.patient_info.last_name}{' '}
@@ -233,17 +233,28 @@ const DoctorSchedule = (props) => {
                                        throat and chest
                                     </td>
                                     <td
-                                       style={{
-                                          color: '#EC6464',
-                                          cursor: 'pointer',
-                                       }}
+                                    className={DSstyles.tdCancel}
                                        onClick=""
                                     >
                                        Cancel
                                     </td>
                                  </tr>
                               );
-                           })}
+                           })} */}
+{/*Dummy Texts*/}
+                           <tr>
+                              <td className={DSstyles.imgSection}>
+                                 <img src={avatarOne} alt={'img'} />
+                              </td>
+                              <td className={DSstyles.tdName}>Ama Osaba</td>
+                              <td className={DSstyles.tdCondition}>
+                                 Swollen tonsils with severe pains in throat and
+                                 chest
+                              </td>
+                              <td className={DSstyles.tdCancel} onClick="">
+                                 Cancel
+                              </td>
+                           </tr>
                         </tbody>
                      </table>
                   </div>
