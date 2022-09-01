4ba1e34ff2b7554f2fcf067993e9572ffa00306b
Nii <niiodartey10@gmail.com>
2022-09-01 18:35:36 +0100

    make drag and drop work

--- client/src/Pages/DoctorDashboard/Records/Dropzone.js
+++ client/src/Pages/DoctorDashboard/Records/Dropzone.js
@@ -1,17 +1,39 @@
-import React, { useCallback } from 'react'
+import styles from './DoctorRecords.module.css';
+import React, { useState,useRef,useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { FaFileUpload } from 'react-icons/fa';
 
-function MyDropzone() {
+function Dropzone() {
+    const [selectedFile, setSelectedFile] = useState()
+    const [isSelected, setIsSelected] = useState(false)
+
+
+    const docRecordsUploadRef = useRef()
+
+    const uploadFile = () => {
+        docRecordsUploadRef.current.click()
+    }
+    const changeHandler = (e) => {
+        setSelectedFile(e.target.files[0])
+        setIsSelected(true)
+    }
+
+    const handleSubmission = () => {
+
+    }
+
+
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
-        console.alert(acceptedFiles)
+        alert(acceptedFiles)
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
         <div {...getRootProps()} className={styles.docRecordsUpload}>
             <input {...getInputProps()} ref={docRecordsUploadRef} style={{ display: 'none' }} type="file" accept='.doc,.docx,.pdf,.txt' name='file' onChange={changeHandler} />
-            {isSelected ? (
+            {isSelected && isDragActive ? 
+            (
                 <div>
                     <p>Filename: {selectedFile.name}</p>
                     <p>Filetype: {selectedFile.type}</p>
@@ -20,8 +42,8 @@ function MyDropzone() {
                         {selectedFile.lastModifiedDate.toLocaleDateString()}</p>
                     <button onClick={handleSubmission}>Submit</button>
                 </div>
-            ) : isDragActive ?
-                    <FaFileUpload className={styles.docRecordsUploadimg} onClick={uploadFile} />}
+            ):
+                <FaFileUpload className={styles.docRecordsUploadimg} onClick={uploadFile} />}
             <h2 className={styles.docRecordsSheader}>
                 Drag and drop file or <span className={styles.docRecordsButtonLink} onClick={uploadFile}>browse</span>
                 {/* Drag and drop file or{' '}
@@ -29,9 +51,11 @@ function MyDropzone() {
                      browse
                   </Link> */}
             </h2>
-            :  <span className={styles.docRecordsButtonLink} onClick={uploadFile}>upload</span>
         </div>
     )
 }
+
+
+export default Dropzone;
 
 
