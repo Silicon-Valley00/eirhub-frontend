1409470b1aaa998c256233590f9416f6e8167b0f
JhohannesK <Jdomeh77@gmail.com>
2022-09-18 00:56:04 +0000

    fix hero image

--- client/src/Pages/DocDashboard/Records/DoctorRecords.js
+++ client/src/Pages/DocDashboard/Records/DoctorRecords.js
@@ -5,11 +5,10 @@ import Dropzone from './Dropzone';
 const DoctorRecords = () => {
    return (
       <>
-         <div className={styles.wrapper}>
-            {/* <div className={styles.docRecordsContainer}> */}
-               <div className={styles.docRecordsContainer1}>
-                  <h1>Upload Files</h1>
-                  {/* <div className={styles.docRecordsUpload}>
+         {/* <div className={styles.docRecordsContainer}> */}
+         <div className={styles.docRecordsContainer1}>
+            <h1>Upload Files</h1>
+            {/* <div className={styles.docRecordsUpload}>
                <input ref={docRecordsUploadRef} style={{display:'none'}} type="file" accept='.doc,.docx,.pdf,.txt' name='file' onChange={changeHandler}/>
                {isSelected ? (
                   <div>
@@ -27,32 +26,29 @@ const DoctorRecords = () => {
                   {/* Drag and drop file or{' '}
                </h2>
             </div> */}
-                  <Dropzone />
+            <Dropzone />
 
-                  <div className={styles.docRecordsFiles}>
-                     <table>
-                        <thead>
-                           <th>Title</th>
-                           <th>Type</th>
-                           <th>Date Uploaded</th>
-                           <th>Actions</th>
-                        </thead>
-                        <tbody>
-                           <td>Lab_Report</td>
-                           <td>Lab_Report</td>
-                           <td>07/10/2022</td>
-                           <td className={styles.docRecordsicons}>
-                              <FaPencilAlt
-                                 className={styles.docRecordspencil}
-                              />
-                              <FaTrash className={styles.docRecordstrash} />
-                           </td>
-                           <tr></tr>
-                        </tbody>
-                     </table>
-                  </div>
-               {/* </div> */}
+            <div className={styles.docRecordsFiles}>
+               <table>
+                  <thead>
+                     <th>Title</th>
+                     <th>Type</th>
+                     <th>Date Uploaded</th>
+                     <th>Actions</th>
+                  </thead>
+                  <tbody>
+                     <td>Lab_Report</td>
+                     <td>Lab_Report</td>
+                     <td>07/10/2022</td>
+                     <td className={styles.docRecordsicons}>
+                        <FaPencilAlt className={styles.docRecordspencil} />
+                        <FaTrash className={styles.docRecordstrash} />
+                     </td>
+                     <tr></tr>
+                  </tbody>
+               </table>
             </div>
+            {/* </div> */}
          </div>
       </>
    );
