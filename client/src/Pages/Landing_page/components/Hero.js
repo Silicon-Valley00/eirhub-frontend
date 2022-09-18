a2135813f6409f7ff070240412f8bdefc8d40b6c
JhohannesK <Jdomeh77@gmail.com>
2022-09-18 20:18:22 +0000

    fix alignment

--- client/src/Pages/Landing_page/components/Hero.js
+++ client/src/Pages/Landing_page/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import landingpageImage from '../../../assets/landingpage design.png';
+import landingpageImage from '../../../assets/Reduced_hero.png';
 import heroStyles from './hero.module.css';
 import Typewriter from 'typewriter-effect';
 
--- client/src/Pages/Landing_page/components/Navbar.js
+++ client/src/Pages/Landing_page/components/Navbar.js
@@ -7,23 +7,22 @@ import { Link } from 'react-router-dom';
 import LoginDropdown from './LoginDropdown';
 import SignUpDropdown from './SignUpDropdown';
 
-
 // Function to use when the user clicks outside of the dropdown.
-function useClickOutside(Handler){
+function useClickOutside(Handler) {
    let menuRef = useRef();
    useEffect(() => {
-   let secondHandler = (event) => {
-      if (!menuRef.current.contains(event.target)) {
-         Handler();
-      }
-   };
-   document.addEventListener('mousedown', secondHandler);
-   return () => {
+      let secondHandler = (event) => {
+         if (!menuRef.current.contains(event.target)) {
+            Handler();
+         }
+      };
       document.addEventListener('mousedown', secondHandler);
-   };
-});
-return menuRef;
-};
+      return () => {
+         document.addEventListener('mousedown', secondHandler);
+      };
+   });
+   return menuRef;
+}
 
 const Navbar = (props) => {
    const [sidebar, setSidebar] = useState(false);
@@ -33,133 +32,133 @@ const Navbar = (props) => {
    const showSidebar = () => setSidebar(!sidebar);
    const handleSignUpClick = () => {
       setSignUpClick(!signUpClick);
-      console.log(signUpClick)
+      console.log(signUpClick);
       setLoginClick(false);
    };
    const handleLoginClick = () => {
       setLoginClick(!loginClick);
-      console.log(loginClick)
+      console.log(loginClick);
       setSignUpClick(false);
    };
 
-   let menuRef = useClickOutside(()=>{
+   let menuRef = useClickOutside(() => {
       setLoginClick(false);
       setSignUpClick(false);
-   })
+   });
 
    return (
-      <div className={ styles.nav_background }>
-      <main className={styles.main}>
-         <div className={styles.back} ref={menuRef}>
-            <nav id={styles.nav}>
-               {/* Logo */}
-               <Link to="/" id={styles.img}>
-                  <img
-                     src={logo}
-                     className={styles.logo_img}
-                     alt=""
-                     height={50}
-                     width={200}
-                  />
-               </Link>
-               <div
-                  className={
-                     sidebar
-                        ? `${styles.linkContainer} ${styles.sidebar}`
-                        : `${styles.linkContainer}`
-                  }
-               >
-                  {/* Close icon */}
-                  <div>
+      <div className={styles.nav_background}>
+         <main className={styles.main}>
+            <div className={styles.back} ref={menuRef}>
+               <nav id={styles.nav}>
+                  {/* Logo */}
+                  <Link to="/" id={styles.img}>
                      <img
-                        src={close}
+                        src={logo}
+                        className={styles.logo_img}
                         alt=""
-                        id={styles.close}
-                        height={20}
-                        width={20}
-                        onClick={showSidebar}
+                        height={50}
+                        width={200}
                      />
-                  </div>
-                  <div className={styles.left}>
-                     <ul id={styles.nav_links}>
-                        <Link
-                           to="/"
-                           className={
-                              props.indicator === 1
-                                 ? `${styles.active} ${styles.each}`
-                                 : styles.each
-                           }
-                        >
-                           Home
-                        </Link>
-                        <Link
-                           to="/our-services"
-                           className={
-                              props.indicator === 2
-                                 ? `${styles.active} ${styles.each}`
-                                 : styles.each
-                           }
-                        >
-                           Our Services
-                        </Link>
-                        <Link
-                           to="/how-it-works"
-                           className={
-                              props.indicator === 3
-                                 ? `${styles.active} ${styles.each}`
-                                 : styles.each
-                           }
-                        >
-                           How it Works
-                        </Link>
-                     </ul>
-                     <div id={styles.signup}>
-                        <ul className={styles.signup_list}>
-                           <li
-                              className={styles.signup_item}
-                              onClick={handleSignUpClick}
+                  </Link>
+                  <div
+                     className={
+                        sidebar
+                           ? `${styles.linkContainer} ${styles.sidebar}`
+                           : `${styles.linkContainer}`
+                     }
+                  >
+                     {/* Close icon */}
+                     <div>
+                        <img
+                           src={close}
+                           alt=""
+                           id={styles.close}
+                           height={20}
+                           width={20}
+                           onClick={showSidebar}
+                        />
+                     </div>
+                     <div className={styles.left}>
+                        <ul id={styles.nav_links}>
+                           <Link
+                              to="/"
+                              className={
+                                 props.indicator === 1
+                                    ? `${styles.active} ${styles.each}`
+                                    : styles.each
+                              }
                            >
-                              Sign Up
-                           </li>
-                           <li id={styles.separator}>|</li>
-                           <li
-                              className={styles.signup_item}
-                              onClick={handleLoginClick}
+                              Home
+                           </Link>
+                           <Link
+                              to="/our-services"
+                              className={
+                                 props.indicator === 2
+                                    ? `${styles.active} ${styles.each}`
+                                    : styles.each
+                              }
                            >
-                              Login
-                           </li>
+                              Our Services
+                           </Link>
+                           <Link
+                              to="/how-it-works"
+                              className={
+                                 props.indicator === 3
+                                    ? `${styles.active} ${styles.each}`
+                                    : styles.each
+                              }
+                           >
+                              How it Works
+                           </Link>
                         </ul>
-                        {loginClick && (
-                           <LoginDropdown
-                              handleModalLogin={props.handleModalLogin}
-                              handleModalLoginDoctor={
-                                 props.handleModalLoginDoctor
-                              }
-                           />
-                        )}
-                        {signUpClick && (
-                           <SignUpDropdown
-                              handleModalSignup={props.handleModalSignup}
-                              handleModalSignupDoctor={
-                                 props.handleModalSignupDoctor
-                              }
-                           />
-                        )}
+                        <div id={styles.signup}>
+                           <ul className={styles.signup_list}>
+                              <li
+                                 className={styles.signup_item}
+                                 onClick={handleSignUpClick}
+                              >
+                                 Sign Up
+                              </li>
+                              <li id={styles.separator}>|</li>
+                              <li
+                                 className={styles.signup_item}
+                                 onClick={handleLoginClick}
+                              >
+                                 Login
+                              </li>
+                           </ul>
+                           {loginClick && (
+                              <LoginDropdown
+                                 handleModalLogin={props.handleModalLogin}
+                                 handleModalLoginDoctor={
+                                    props.handleModalLoginDoctor
+                                 }
+                              />
+                           )}
+                           {signUpClick && (
+                              <SignUpDropdown
+                                 handleModalSignup={props.handleModalSignup}
+                                 handleModalSignupDoctor={
+                                    props.handleModalSignupDoctor
+                                 }
+                              />
+                           )}
+                        </div>
                      </div>
                   </div>
-               </div>
-               {/* menu icon */}
-               <img
-                  src={menu}
-                  alt=""
-                  height={20}
-                  width={20}
-                  id={styles.menu}
-                  onClick={showSidebar}
-               />
-            </nav>
-         </div>
-      </main>
+                  {/* menu icon */}
+                  <img
+                     src={menu}
+                     alt=""
+                     height={20}
+                     width={20}
+                     id={styles.menu}
+                     onClick={showSidebar}
+                  />
+               </nav>
+            </div>
+         </main>
       </div>
    );
 };
