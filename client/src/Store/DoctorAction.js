14af5b9df41497f93f48251cd77ca0ddc4133c32
Rexford Machu <rexfordmachu@gmail.com>
2022-09-20 22:30:56 +0000

    Replace Headers with variable in file.
    Replace Headers with variable in file.

--- client/src/Store/DoctorAction.js
+++ client/src/Store/DoctorAction.js
@@ -31,68 +31,102 @@ export const setDoctorAuth = (auth) => {
    };
 };
 
+//Headers for API Call.
+const headers = {
+   'Access-Control-Allow-Origin': '*',
+   'Access-Control-Allow-Headers': '*',
+   'Access-Control-Allow-Methods': '*',
+}
+const baseURL = 'http://127.0.0.1:5000'
 // Action creator: Fetch doctor profile info
 export const fetchDoctorsProfileInfo = (idDoctor) => {
    return async function (dispatch) {
-      try {
-         const response = await axios({
-            method: 'GET',
-            url: `http://127.0.0.1:5000/doctor/${idDoctor}`,
-            headers: {
-               'Access-Control-Allow-Origin': '*',
-               //Helpful in some cases.
-               'Access-Control-Allow-Headers': '*',
-               'Access-Control-Allow-Methods': '*',
-            },
-         });
-         if (response.status === 200) {
-            //checks details of response
-            if (response.data.status === true) {
-               //returns response
-               // alert('doctor profile fetched');
-               dispatch(setDoctorProfile(response.data.msg));
-               dispatch(setOkToRoute(true));
-            }
-         } else {
-            //takes all statuses aside 200
-            // alert('Something went wrong. Try again');
+      await axios.get(
+         `${baseURL}/doctor/${idDoctor}`,
+         {
+            headers
          }
-      } catch (error) {
-         // alert('caughterror', error);
+      )
+      .then((response)=>{
+         if (response.data.status === true) {
+            //returns response
+            // alert('doctor profile fetched');
+            dispatch(setDoctorProfile(response.data.msg));
+            dispatch(setOkToRoute(true));
+         }
+         else{
+
+         }
+      })
+      .catch((error)=>{
          console.log(error);
-      }
+      })
+
+      //       try {
+      //    const response = await axios({
+      //       method: 'GET',
+      //       url:,
+      //       headers: headers,
+      //    });
+      //    if (response.status === 200) {
+      //       //checks details of response
+      //       if (response.data.status === true) {
+      //          //returns response
+      //          // alert('doctor profile fetched');
+      //          dispatch(setDoctorProfile(response.data.msg));
+      //          dispatch(setOkToRoute(true));
+      //       }
+      //    } else {
+      //       //takes all statuses aside 200
+      //       // alert('Something went wrong. Try again');
+      //    }
+      // } catch (error) {
+      //    // alert('caughterror', error);
+      //    console.log(error);
+      // }
    };
 };
 
 //Fectches patients by doctor id
 export async function fetchPatientsByDoctorId(id_doctor) {
-   try {
-      
-      const response = await axios({
-         method: 'GET',
-         url: `http://127.0.0.1:5000/doctors/?id_doctor=${id_doctor}`,
-         headers: {
-            'Access-Control-Allow-Origin': '*',
-            //Helpful in some cases.
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': '*',
-         },
-      });
-      if (response.status === 200) {
-         //checks details of response
-
-         if (response.data) {
-            //returns response
-            // alert('patients by doctor id worked fetch worked');            
-            return response.data.msg;
-         }
-      } else {
-         //takes all statuses aside 200
-         alert('Something went wrong. Try again, doctor 1');
+   await axios.get(
+      `${baseURL}/doctors/?id_doctor=${id_doctor}`,
+      {
+         headers
       }
-   } catch (error) {
-      // alert(error, 'doctors 2');
-   }
+   )
+   .then((response)=> {
+      if (response.data) {
+         //returns response
+         // alert('patients by doctor id worked fetch worked');            
+         return response.data.msg;
+      }
+   })
+   .catch((error)=>{
+      console.log(error);
+   })
+   // try {
+   //    const response = await axios({
+   //       method: 'GET',
+   //       url: `http://127.0.0.1:5000/doctors/?id_doctor=${id_doctor}`,
+   //       headers: headers,
+   //    });
+   //    if (response.status === 200) {
+   //       //checks details of response
+
+   //       if (response.data) {
+   //          //returns response
+   //          // alert('patients by doctor id worked fetch worked');            
+   //          return response.data.msg;
+   //       }
+   //    } 
+   //    else {
+   //       //takes all statuses aside 200
+   //       alert('Something went wrong. Try again, doctor 1');
+   //    }
+   // } catch (error) {
+   //    // alert(error, 'doctors 2');
+   // }
 }
 
 // Update Doctor profile
