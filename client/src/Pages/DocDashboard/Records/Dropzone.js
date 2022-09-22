56f99e5088f62afe32ae2423cb11071422633050
Nii <niiodartey10@gmail.com>
2022-09-22 03:23:49 +0100

    finish dropzone

--- client/src/Pages/DocDashboard/Records/Dropzone.js
+++ client/src/Pages/DocDashboard/Records/Dropzone.js
@@ -20,7 +20,7 @@ function Dropzone() {
    // };
 
 
-   function postReport(report_url){
+   function postReport(report_url) {
       const reportData = {
          "report_type": "Lab report",
          "description": `Lab report posted by Cloudinary`,
@@ -31,7 +31,7 @@ function Dropzone() {
          .then(() => {
             alert('Report Uploaded')
          })
-         .catch((error)=>{
+         .catch((error) => {
             alert(`Failed:${error}`)
          })
    }
@@ -40,31 +40,30 @@ function Dropzone() {
 
 
    const handleSubmission = () => {
-      selectedFiles.map(file=>{
+
+      selectedFiles.map(file => {
 
          const formData = new FormData();
          formData.append('file', file);
          formData.append('upload_preset', 'ji5ue4f9')
-         
+
          axios
-         .post('https://api.cloudinary.com/v1_1/eirhub-siliconvalley/image/upload', formData)
-         .then((response) => {
-            postReport(response.data.url)
-         })
-         .catch((error) => console.log(error));
+            .post('https://api.cloudinary.com/v1_1/eirhub-siliconvalley/image/upload', formData)
+            .then((response) => {
+               postReport(response.data.url)
+            })
+            .catch((error) => console.log(error));
       })
-    };
+      setSelectedFiles([])
+      setIsSelected(false)
+      // console.log(isSelected,selectedFiles);
+
+   };
 
    const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
-      // setSelectedFiles(acceptedFiles)
       // Do something with the files'
       acceptedFiles.forEach(file => {
-         const reader = new FileReader()
-         reader.onload = ()=>{
-            
-            setSelectedFiles(previous => [...previous,file])
-         }
-         reader.readAsDataURL(file)
+         setSelectedFiles(previous => [...previous, file])
       })
       setIsSelected(true)
       // console.log(selectedFiles,acceptedFiles, rejectedFiles)
@@ -74,55 +73,67 @@ function Dropzone() {
    }, []);
    const { getRootProps, getInputProps, isDragActive } = useDropzone({
       onDrop,
-      accept: {'text/*': ['.txt','.pdf','.docx','.doc']},
+      accept: { 'text/*': ['.txt', '.pdf', '.docx', '.doc'] },
       multiple: true,
       maxFiles: 3
    });
+   // useDropzone.on('maxfilesreached', function () {
+   //    useDropzone.removeEventListeners();
+   // });
+   // useDropzone.on('removedfile', function (file) {
+   //    useDropzone.setupEventListeners();
+   // });
 
-   useEffect(()=>{
-      console.log(isSelected,selectedFiles)
-   },[selectedFiles])
+   // useEffect(() => {
+   //    console.log(isSelected, selectedFiles)
+   // }, [selectedFiles])
 
    return (
-      <div {...getRootProps()} className={styles.docRecordsUpload}>
-         <input
-            {...getInputProps()}
-            ref={docRecordsUploadRef}
-            style={{ display: 'none' }}
-            type="file"
-            accept=".doc,.docx,.pdf,.txt"
-            name="file"
-         // disabled={isSelected}
-         />
-         {isSelected && (selectedFiles.length > 0) ?
-         <div>
-         <ul /*className={styles.selectedFiles}*/>
-            {selectedFiles.map((file,index) => <li key={index}>
-               <p>Filename: {file.name} Filetype: {file.type} Size: {(file.size / 1024 / 1024).toString().slice(0, 4)}MB</p>
-               </li>
-            )
-            }
-         </ul>
-            <button className={styles.btn} onClick={handleSubmission}>Submit</button>
-      </div>
-            : isDragActive ?
-               <h2>Drop files here</h2>
-               : <div>
-                  <FaFileUpload
-                     className={styles.docRecordsUploadimg}
-                  />
-                  <h2 className={styles.docRecordsSheader}>
-                     Drag and drop file or{' '}
-                     <span className={styles.docRecordsButtonLink} >
-                        browse
+      <div className={styles.dropzone}>
+         <div {...getRootProps()} className={styles.docRecordsUpload}>
+            <input
+               {...getInputProps()}
+               ref={docRecordsUploadRef}
+               style={{ display: 'none' }}
+               type="file"
+               accept=".doc,.docx,.pdf,.txt"
+               name="file"
+               disabled
+            />
+            {isSelected && (selectedFiles.length > 0) ?
+               <div>
+                  <ul /*className={styles.selectedFiles}*/>
+                     {selectedFiles.map((file, index) => <li key={index}>
+                        <p>
+                           Filename: {file.name}
+                           Filetype: {file.type}
+                           Size: {(file.size / 1024 / 1024).toString().slice(0, 4)}MB
+                     </p>
+                     </li>
+                     )
+                     }
+                  </ul>
+               </div>
+               : isDragActive ?
+                  <h2>Drop files here</h2>
+                  : <div>
+                     <FaFileUpload
+                        className={styles.docRecordsUploadimg}
+                     />
+                     <h2 className={styles.docRecordsSheader}>
+                        Drag and drop file or{' '}
+                        <span className={styles.docRecordsButtonLink} >
+                           browse
             </span>
-                     {/* Drag and drop file or{' '}
+                        {/* Drag and drop file or{' '}
             <Link to="/" className={styles.docRecordsButtonLink}>
             browse
          </Link> */}
-                  </h2>
-               </div>
-         }
+                     </h2>
+                  </div>
+            }
+         </div>
+         {isSelected && <button className={styles.btn} onClick={handleSubmission}>Submit</button>}
       </div>
    );
 }
