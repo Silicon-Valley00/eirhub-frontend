058da27e43281ba91cd594a80a05d26ac3f1a559
JhohannesK <jdomeh77@gmail.com>
2022-12-21 17:01:54 +0000

    change CRA and replace with vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { CometChat } from '@cometchat-pro/chat';
-import * as CONSTANTS from './constants/constants';
-import store, { persistor } from '../src/Store/ReducerStore';
+
+// import store, { persistor } from './Store/ReducerStore';
 import { PersistGate } from 'redux-persist/integration/react';
+
+import { APP_ID, APP_REGION } from './constants/constants';
+import store, { persistor } from './Store/ReducerStore';
 import ScrollToTop from './utils/scrollToTop';
 
 let appSetting = new CometChat.AppSettingsBuilder()
    .subscribePresenceForAllUsers()
-   .setRegion(CONSTANTS.APP_REGION)
+   .setRegion(APP_REGION)
    .autoEstablishSocketConnection(true)
    .build();
 
-CometChat.init(CONSTANTS.APP_ID, appSetting).then(
+CometChat.init(APP_ID, appSetting).then(
    () => {
       ReactDOM.render(
          <BrowserRouter>
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [react(), viteTsconfigPaths(), pluginRewriteAll(), svgrPlugin()],
+});
